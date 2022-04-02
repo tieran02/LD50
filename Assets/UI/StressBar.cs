ec11aff585944936320db785c65a53d37c34dfc1
DukeOfJamming <DukeOfJamming@users.noreply.github.com>
2022-04-02 17:26:14 +0100

    Stress Bar, accessor class and art assets done.

---
+++ Assets/UI/StressBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StressBar : MonoBehaviour
+{
+
+    private Slider slider;
+
+    public void SetMaxStress(int stress)
+    {
+        slider.maxValue = stress;
+        slider.value = stress;
+    }
+    public void setStress(int stress);
+    {
+        slider.value = stress;
+    }
+}
