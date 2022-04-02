d94a248c1c593c7bc6b670247b464f3126bf850a
DukeOfJamming <DukeOfJamming@users.noreply.github.com>
2022-04-03 00:13:35 +0100

    Fine Tuned Clock, Fixed y axis at -1 for the player currently to test. It seems to be related to the physics of the player model having forces applied on it from colliding with the environment.

--- Assets/UI/Clock.cs
+++ Assets/UI/Clock.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Clock : MonoBehaviour
 {
     private Text clockText;
-    private float timer = 28800;
+    private float timer = 32400;
     private int hours;
     private int minutes;
 
@@ -22,7 +22,7 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime*60;
+        timer += Time.deltaTime*120;
         minutes = (((int)timer)%3600)/60;
         hours = (((int)timer)/3600)%24;
         clockText.text = hours.ToString().PadLeft(2,'0')+ ":" + minutes.ToString().PadLeft(2,'0');
