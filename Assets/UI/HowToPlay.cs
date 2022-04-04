ec9512a912a3c8c6bc4a8e844016d947217d6326
tieran02 <tieran.wightman@gmail.com>
2022-04-04 19:11:19 +0100

    updated howto play script

--- Assets/UI/HowToPlay.cs
+++ Assets/UI/HowToPlay.cs
@@ -10,19 +10,27 @@ public class HowToPlay : MonoBehaviour
     private void Awake()
     {
         eventSystem = FindObjectOfType<EventSystem>();
-        gameObject.SetActive(false);
+        int currentShift = PlayerPrefs.GetInt("CurrentShift");
+        if(currentShift == 0)
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Time.timeScale = 0.0f;
         if (eventSystem)
             eventSystem.enabled = false;
         if (Input.anyKeyDown)
         {
             if(eventSystem)
                 eventSystem.enabled = true;
+
             gameObject.SetActive(false);
+            if (!FindObjectOfType<PauseMenu>().menu.activeSelf)
+                Time.timeScale = 1.0f;
         }
     }
 }
